612ba6ec6789e8969c63a677c6b3b32b5f0603be
ernestknourov <100434509+ernestknourov@users.noreply.github.com>
2022-11-21 12:12:11 +0300

    Add files via upload

--- task-6-snowflake.py
+++ task-6-snowflake.py
@@ -3,9 +3,10 @@ from snowflake.connector.pandas_tools import write_pandas
 import os
 import pandas as pd
 import pendulum
-from airflow.decorators import dag, task
+from airflow.decorators import task
+from airflow import DAG
 from dotenv import load_dotenv
-
+import config
 
 load_dotenv()
 
@@ -18,80 +19,30 @@ SCHEMA = os.getenv('SCHEMA')
 PATH_TO_CSV = os.getenv('PATH_TO_CSV')
 
 
-table_schema = """
-    "_ID" VARCHAR(16777216),
-    "IOS_APP_ID" BIGINT,
-    "TITLE" VARCHAR(16777216),
-    "DEVELOPER_NAME" VARCHAR(16777216),
-    "DEVELOPER_IOS_ID" BIGINT,
-    "IOS_STORE_URL" VARCHAR(16777216),
-    "SELLER_OFFICIAL_WEBSITE" VARCHAR(16777216),
-    "AGE_RATING" VARCHAR(16777216),
-    "TOTAL_AVERAGE_RATING" FLOAT,
-    "TOTAL_NUMBER_OF_RATINGS" FLOAT,
-    "AVERAGE_RATING_FOR_VERSION" FLOAT,
-    "NUMBER_OF_RATINGS_FOR_VERSION" INT,
-    "ORIGINAL_RELEASE_DATE" VARCHAR(16777216),
-    "CURRENT_VERSION_RELEASE_DATE" VARCHAR(16777216),
-    "PRICE_USD" FLOAT,
-    "PRIMARY_GENRE" VARCHAR(16777216),
-    "ALL_GENRES" VARCHAR(16777216),
-    "LANGUAGES" VARCHAR(16777216),
-    "DESCRIPTION" VARCHAR(16777216)
-    """
-
-table_columns = """
-    "_ID",
-    "IOS_APP_ID",
-    "TITLE",
-    "DEVELOPER_NAME",
-    "DEVELOPER_IOS_ID",
-    "IOS_STORE_URL",
-    "SELLER_OFFICIAL_WEBSITE",
-    "AGE_RATING",
-    "TOTAL_AVERAGE_RATING",
-    "TOTAL_NUMBER_OF_RATINGS",
-    "AVERAGE_RATING_FOR_VERSION",
-    "NUMBER_OF_RATINGS_FOR_VERSION",
-    "ORIGINAL_RELEASE_DATE",
-    "CURRENT_VERSION_RELEASE_DATE",
-    "PRICE_USD" FLOAT,
-    "PRIMARY_GENRE",
-    "ALL_GENRES",
-    "LANGUAGES",
-    "DESCRIPTION"
-    """
+def create_connection() -> snowflake.connector.connection:
+    connection = snowflake.connector.connect(
+        user=USER,
+        password=PASSWORD,
+        account=ACCOUNT,
+        warehouse=WAREHOUSE,
+        database=DATABASE,
+        schema=SCHEMA
+    )
+    return connection
 
 
-@dag(
-    schedule=None,
-    start_date=pendulum.datetime(2023, 11, 10, tz="UTC"),
-    catchup=False,
-    description='task-6-snowflake',
-)
-def pipeline() -> None:
-    """
-    This is pipeline for the 6th task. It gets data from file,
-    create tables and streams, write data throughout all tables
-    into master table. All actions are performed using Airflow.
-    """
-
-    def create_connection() -> snowflake.connector.connection:
-        connection = snowflake.connector.connect(
-            user=USER,
-            password=PASSWORD,
-            account=ACCOUNT,
-            warehouse=WAREHOUSE,
-            database=DATABASE,
-            schema=SCHEMA
-        )
-        return connection
-
+with DAG(
+        dag_id="task-6-snowflake",
+        schedule=None,
+        start_date=pendulum.datetime(2023, 11, 10, tz="UTC"),
+        catchup=False,
+        description='task-6-snowflake',
+) as dag:
     @task()
     def create_table_and_streams(cursor: snowflake.connector.cursor) -> bool:
         """Create tables and streams in our storage"""
         create_raw_table = f"""CREATE OR REPLACE TABLE 
-                            {DATABASE}.{SCHEMA}.RAW_TABLE ({table_schema})"""
+                            {DATABASE}.{SCHEMA}.RAW_TABLE ({config.table_schema})"""
 
         cursor.execute(create_raw_table)
         cursor.execute("CREATE OR REPLACE TABLE STAGE_TABLE LIKE RAW_TABLE")
@@ -101,6 +52,7 @@ def pipeline() -> None:
         cursor.execute("CREATE OR REPLACE STREAM STAGE_STREAM ON TABLE STAGE_TABLE")
         return True
 
+
     @task()
     def insert_into_raw_table(connection: snowflake.connector.connection, path_to_csv: str) -> bool:
         """Insert raw data into snowflake table"""
@@ -109,17 +61,20 @@ def pipeline() -> None:
         write_pandas(connection, data, table_name='RAW_TABLE')
         return True
 
+
     @task()
     def insert_into_stage_table(cursor: snowflake.connector.cursor) -> bool:
         """Insert data into STAGE_TABLE from raw-stream"""
-        cursor.execute(f"INSERT INTO STAGE_TABLE (SELECT {table_columns} FROM RAW_STREAM)")
+        cursor.execute(f"INSERT INTO STAGE_TABLE (SELECT {config.table_columns} FROM RAW_STREAM)")
         return True
+
 
     @task()
     def insert_into_master_table(cursor: snowflake.connector.cursor) -> bool:
         """Insert data into MASTER_TABLE from stage-stream"""
-        cursor.execute(f"INSERT INTO MASTER_TABLE (SELECT {table_columns} FROM STAGE_STREAM)")
+        cursor.execute(f"INSERT INTO MASTER_TABLE (SELECT {config.table_columns} FROM STAGE_STREAM)")
         return True
+
 
     connection = create_connection()
     cursor = connection.cursor()
@@ -128,7 +83,3 @@ def pipeline() -> None:
     insert_into_raw_table(connection, PATH_TO_CSV)
     insert_into_stage_table(cursor)
     insert_into_master_table(cursor)
-
-
-if __name__ == '__main__':
-    pipeline()
